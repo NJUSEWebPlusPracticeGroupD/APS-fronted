ff65398f090dd11b5bcb2d3deabd40ac0ed8dade
咖啡馆大叔 <oncwnuKWjIFGR8CD0jinmRkRebrs@git.weixin.qq.com>
2020-11-04 22:01:20 +0800

    应该不用删除about.vue?

---
+++ src/views/about.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-main class="main">
+    <h2>NJUERP</h2>
+    <span>我们致力于打造新型的工厂排程系统</span>
+  </el-main>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+
+  .main{
+    background-color: #FFFFFF;
+    margin:20px 20px;
+    padding: 40px;
+    height: 400px;
+    border-radius: 10px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, .12), 0 0 12px rgba(0, 0, 0, .04);
+  }
+  .main:hover{
+    margin:10px 20px;
+  }
+  .main h2{
+    font-family: serif;
+    font-size: 60px;
+  }
+  .main span{
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+    font-size: 20px;
+  }
+
+</style>
