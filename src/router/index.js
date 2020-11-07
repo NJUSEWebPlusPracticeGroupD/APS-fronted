59fdb6f6dbdd5288c08eb1e9fd243193d90689d6
SCfive <1320163325@qq.com>
2020-11-07 13:40:41 +0800

    规范文件命名

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ export const constantRoutes = [
   {
     path:'/about',
     name:'about',
-    component:() => import('../views/about.vue')
+    component:() => import('../views/About.vue')
   },
   {
     path:'/orderGantt',
