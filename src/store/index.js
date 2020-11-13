4761496c7f231721b60cdf918c7cd617f1069262
tkTywin <tywin0302@gmail.com>
2020-11-13 13:17:41 +0800

    [fix]修复排程时间的Bug

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
 const state={   //要设置的全局访问的state对象
-  time: '2018-11-01 07:00:00',
+  time: '2018-09-07 12:00:00',
   timesOfSecond:1
 }
 const getters = {   //实时监听state值的变化(最新状态)
